a213df9ba4d833495a304293336231e71bee2a26
Matias Grioni <matgrioni@gmail.com>
2015-06-24 10:36:29 -0400

    Introduction message for script

---
+++ 6/Python/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+print("This program finds the difference between the sum of squares of the")
+print("first n natural numbers and the square of the sum of the first n")
+print("natural numbers")
