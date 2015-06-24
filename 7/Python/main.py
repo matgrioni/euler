7f561fa162cb0d8e30a27f0f64a114bcf3600694
Matias Grioni <matgrioni@gmail.com>
2015-06-24 11:14:35 -0400

    Changed upper bound on multiples from current / 2 to sqrt(current)

--- 7/Python/main.py
+++ 7/Python/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import time
+import math
 
 print("This program finds the nth prime number")
 n = int(raw_input("Enter n > "))
@@ -13,7 +14,7 @@ while count < n:
     current += 1
 
     prime = True
-    for i in range(2, current/2 + 1):
+    for i in range(2, int(math.sqrt(current)) + 1):
         if current % i == 0:
             prime = False
             break
