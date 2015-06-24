8ada0a55405c7dadc453ea5ae3eb59bcea75921a
Matias Grioni <matgrioni@gmail.com>
2015-06-24 13:25:53 -0400

    Increased performance

--- 9/Python/main.py
+++ 9/Python/main.py
@@ -2,18 +2,22 @@
 
 import time
 
+def triplet(n):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            for c in range(1, n+1):
+                if a + b + c == n and a*a + b*b == c*c:
+                    return (a, b, c)
+
+                if a + b+ c > n:
+                    break
+                    
+
 print("Returns a pythagorean triplet where a + b + c = n, and calculates abc")
 n = int(raw_input("Enter n > "))
 
 start = time.time()
-answer = ()
-
-for a in range(1, n+1):
-    for b in range(1, n+1):
-        for c in range(1, n+1):
-            if a + b + c == n and a*a + b*b == c*c:
-                answer = (a, b, c)
-                break
+answer = triplet(n)
 end = time.time()
 
 print("Result: {}".format(answer))
