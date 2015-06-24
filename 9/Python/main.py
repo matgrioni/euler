a389a789b4b04d2ebddf5496ab9c5fa19464284e
Matias Grioni <matgrioni@gmail.com>
2015-06-24 12:45:28 -0400

    Initial solution

---
+++ 9/Python/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import time
+
+print("Returns a pythagorean triplet where a + b + c = n, and calculates abc")
+n = int(raw_input("Enter n > "))
+
+answer = ()
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        for c in range(1, n+1):
+            if a + b + c == n and a*a + b*b == c*c:
+                answer = (a, b, c)
+                break
+
+print("Result: {}".format(answer))
+print("abc = {}".format(answer[0] * answer[1] * answer[2]))
