0e6aa110c75edf92429ba5d1b230356af6dfd25e
Matias Grioni <matgrioni@gmail.com>
2015-06-24 13:28:58 -0400

    Performance change, but more important for very large n

--- 9/Python/main.py
+++ 9/Python/main.py
@@ -5,6 +5,9 @@ import time
 def triplet(n):
     for a in range(1, n+1):
         for b in range(1, n+1):
+            if a + b > n:
+                break
+
             for c in range(1, n+1):
                 if a + b + c == n and a*a + b*b == c*c:
                     return (a, b, c)
