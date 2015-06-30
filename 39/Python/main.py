67dac6f51ca5a3ddf5ded1ac29678e0e1165baac
Matias Grioni <matgrioni@gmail.com>
2015-06-30 15:32:26 -0400

    Redid problem 39

--- 39/Python/main.py
+++ 39/Python/main.py
@@ -10,11 +10,10 @@ start = time.time()
 count = 0
 m = 0
 res = 0
-for p in range(1, n):
+for p in range(3, n+1):
     for a in range(1, p):
-        for b in range(1, p-a):
-            for c in range(1, p-b-a):
-
+        for b in range(1, p-a+1):
+            for c in range(1, p-a-b+1):
                 if a * a + b * b == c * c and a + b + c == p:
                     count += 1
 
