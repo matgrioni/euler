f02bd257db5a786863a5e27bbdef88a52d2b5865
Matias Grioni <matgrioni@gmail.com>
2015-07-02 14:06:18 -0400

    Removed parenthesis and added input

--- 1/Haskell/main.hs
+++ 1/Haskell/main.hs
@@ -1,4 +1,9 @@
 module Main where
 
 main = do
-    print (sum  (filter (\n -> n `mod` 3 == 0 || n `mod` 5 == 0) [1..999]))
+    putStrLn "Sums digits from 1 to n divisible by 3 or 5"
+    putStrLn "Enter n > "
+    input <- getLine
+    let n = read input :: Int
+
+    print $ sum $ filter (\a -> a `mod` 3 == 0 || a `mod` 5 == 0) [1..n-1]
