e4a95cde610a7607ec839cf0bc39a3ac9bcf0f67
Matias Grioni <matgrioni@gmail.com>
2015-07-07 10:35:04 -0400

    Cleaned up redudancies and added comments

--- 2/Haskell/main.hs
+++ 2/Haskell/main.hs
@@ -1,14 +1,14 @@
-{-|
+{-
  - Author: Matias Grioni
  - Created: 7/2/2015
  -
  - Solution to Project Euler problem 2 using Haskell. Sums all even valued
  - terms of the Fibonacci sequence less than n, which is input by the user.
- - DOES NOT WORK.
  -}
 
 module Main where
 
+-- Fibonacci sequence up to n, assuming n is a natural number.
 fibonacci :: Int -> [Int]
 fibonacci 1 = [1]
 fibonacci 2 = [2, 1]
@@ -22,6 +22,9 @@ main = do
     putStrLn "Enter n > "
     input <- getLine
     let n = read input :: Int
-    print $ sum $ map head $ filter ((== 0) . (`mod` 2) . head) $ takeWhile ((< n) . head) [fibonacci n | n <- [1..]]
 
-    --print $ sum $ filter ((==0) . (`mod` 2)) $ takeWhile (<n) . head $ [fibonacci n | n <- [1..]]
+    -- Finds the sum of all fibonacci terms less than n that are even
+    -- Very inefficient as for each term, the entire fibonacci sequence is
+    -- generated and the head of the list is taken. Come back later once
+    -- I'm more familiar with the language.
+    print $ sum $ filter ((== 0) . (`mod` 2)) $ takeWhile (< n) [head (fibonacci n) | n <- [1..]]
