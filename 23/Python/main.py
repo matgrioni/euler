fa724347b5d3fb70f7f925fe365ca80c0e449a00
Matias Grioni <matgrioni@gmail.com>
2015-07-17 17:14:41 -0400

    Improved efficiency Used sqrt rather than /2 for abundant check and for sum check created set of all abundant numbers less than the upper limit. Then found all pairs of numbers which summed to a number to be checked. The pairs were checked by seeing if they existed in the set.

--- 23/Python/main.py
+++ 23/Python/main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
 
 import time
+import math
 
 # Returns if the given number is an abundant number or not
 def isAbundant(n):
-    # Check through all possible proper factors
-    # TODO: Use sqrt as limit instead to be more efficient
-    s = 0
-    for i in range(1, n/2 + 1):
+    sqrt = int(math.sqrt(n))
+
+    # Check through all possible proper factors. One is always present
+    # so start the sum there, and start checking from 2.
+    s = 1
+    for i in range(2, sqrt + 1):
         if n % i == 0:
             s += i
+            s += n / i
+
+    # Since we check for the square root in the range above, we would
+    # add the sqrt twice, so if this sqrt is not a rounded decimal
+    # then subtract the duplicate
+    if sqrt * sqrt == n:
+        s -= sqrt
 
     return s > n
 
@@ -17,6 +27,8 @@ print("Finds the sum of all numbers that can't be written as a sum of")
 print("abundant numbers")
 
 start = time.time()
+
+abundants = {i for i in range(12, 28112) if isAbundant(i)}
 
 # All numbers greater than 28123 can be writtten as a sum of abundant numbers
 # so only go up to 28123.
@@ -32,7 +44,7 @@ for i in range(1, 28124):
     # numbers. True if it can be, false if i can not be written like that.
     sumAbundant = False
     for j in range(12, i / 2 + 1):
-        if isAbundant(j) and isAbundant(i - j):
+        if j in abundants and (i-j) in abundants:
             sumAbundant = True
             break
     
