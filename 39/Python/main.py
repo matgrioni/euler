c075364ee14fce746a57837fcdf78e83049c763c
Matias Grioni <matgrioni@gmail.com>
2016-07-08 22:11:57 -0400

    Same time complexity but lower coefficient

--- 39/Python/main.py
+++ 39/Python/main.py
@@ -4,22 +4,23 @@ import time
 import math
 
 print("Finds the maximum number of pythagorean triplets with a given perimeter <= p")
-p = int(raw_input("Enter p > "))
+perimeter = int(raw_input("Enter p > "))
 
 start = time.time()
 
 # Initialize counter array
-counts = [0] * (p + 1)
+counts = [0] * (perimeter + 1)
 
-for a in range(p):
-    for b in range(p - a):
-        csq = a * a + b * b
-        c = math.sqrt(csq)
-
-        if c.is_integer():
-            ci = int(c)
-            if a + b + c <= p:
-                counts[a + b + ci] += 1
+# Perimeter must be even in a right triangle and p = a + b + c and
+# a^2 + b^2 = c^2 substitution was used to find rem. This results in
+# p * (p - 1 * a) / (2 * (p - a)) which must be an integer in a right
+# triangle.
+for p in range(2, perimeter + 1, 2):
+    for a in range(1, p - 1):
+        rem = p * (p - 2 * a) % (2 * (p - a))
+        
+        if rem == 0:
+            counts[p] += 1
 
 res = counts.index(max(counts))
 end = time.time()
